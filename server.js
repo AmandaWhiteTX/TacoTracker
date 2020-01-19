6b6f9a9006841f596e551b6deefc5fd7a519e766
AmandaWhiteTX <54691794+AmandaWhiteTX@users.noreply.github.com>
2020-01-18 20:40:45 -0600

    Updated Files in Repository

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //import routes and give the server access to them
-var routes = require("./controllers/tacossController.js");
+var routes = require("./controllers/tacosController.js");
 
 app.use(routes);
 
